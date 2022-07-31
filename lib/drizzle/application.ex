90e2f969c3464b3a73b6f82a5ab2ced26cd69e62
Troy Spruit <troy@collectiveidea.com>
2022-07-30 21:55:11 -0400

    Update packages to newer versions. Working with Elixir elixir 1.12.3-otp-24 and erlang 24.3.2.

--- lib/drizzle/application.ex
+++ lib/drizzle/application.ex
@@ -2,11 +2,13 @@ defmodule Drizzle.Application do
   @moduledoc """
   Main application for Drizzle
   """
+
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
 
   use Application
 
+  @impl true
   def start(_type, _args) do
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
@@ -36,6 +38,7 @@ defmodule Drizzle.Application do
 
   # Tell Phoenix to update the endpoint configuration
   # whenever the application is updated.
+  @impl true
   def config_change(changed, _new, removed) do
     DrizzleUiWeb.Endpoint.config_change(changed, removed)
     :ok
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Drizzle.MixProject do
 
   @app :drizzle
   @version "0.1.0"
-  @all_targets [:rpi, :rpi0, :rpi2, :rpi3, :rpi3a, :rpi4, :bbb, :x86_64]
+  @all_targets [:rpi, :rpi0, :rpi2, :rpi3, :rpi3a, :rpi4, :bbb, :osd32mp1, :x86_64, :grisp2]
 
   def project do
     [
@@ -11,21 +11,13 @@ defmodule Drizzle.MixProject do
       version: @version,
       elixir: "~> 1.11",
       compilers: [:phoenix] ++ Mix.compilers(),
-      archives: [nerves_bootstrap: "~> 1.8"],
+      archives: [nerves_bootstrap: "~> 1.11"],
       start_permanent: Mix.env() == :prod,
       build_embedded: true,
-      aliases: [loadconfig: [&bootstrap/1]],
       deps: deps(),
       releases: [{@app, release()}],
       preferred_cli_target: [run: :host, test: :host]
     ]
-  end
-
-  # Starting nerves_bootstrap adds the required aliases to Mix.Project.config()
-  # Aliases are only added if MIX_TARGET is set.
-  def bootstrap(args) do
-    Application.start(:nerves_bootstrap)
-    Mix.Task.run("loadconfig", args)
   end
 
   # Run "mix help compile.app" to learn about applications.
@@ -40,25 +32,22 @@ defmodule Drizzle.MixProject do
   defp deps do
     [
       # Dependencies for all targets
-      {:nerves, "~> 1.8.0", runtime: false},
-      {:shoehorn, "~> 0.6"},
-      {:ring_logger, "~> 0.6"},
-      {:toolshed, "~> 0.2"},
+      {:nerves, "~> 1.7.16 or ~> 1.8.0", runtime: false},
+      {:shoehorn, "~> 0.9.1"},
+      {:ring_logger, "~> 0.8.5"},
+      {:toolshed, "~> 0.2.26"},
 
       # Dependencies for all targets except :host
-      {:nerves_runtime, "~> 0.6", targets: @all_targets},
-      {:nerves_pack, "~> 0.2", targets: @all_targets},
+      {:nerves_runtime, "~> 0.13.0", targets: @all_targets},
+      {:nerves_pack, "~> 0.7.0", targets: @all_targets},
       {:nerves_time, "~> 0.4.0", targets: @all_targets},
 
       # Dependencies for specific targets
-      {:nerves_system_rpi, "~> 1.11", runtime: false, targets: :rpi},
-      {:nerves_system_rpi0, "~> 1.11", runtime: false, targets: :rpi0},
-      {:nerves_system_rpi2, "~> 1.11", runtime: false, targets: :rpi2},
-      {:nerves_system_rpi3, "~> 1.11", runtime: false, targets: :rpi3},
-      {:nerves_system_rpi3a, "~> 1.11", runtime: false, targets: :rpi3a},
-      {:nerves_system_rpi4, "~> 1.11", runtime: false, targets: :rpi4},
-      {:nerves_system_bbb, "~> 2.6", runtime: false, targets: :bbb},
-      {:nerves_system_x86_64, "~> 1.11", runtime: false, targets: :x86_64},
+      # NOTE: It's generally low risk and recommended to follow minor version
+      # bumps to Nerves systems. Since these include Linux kernel and Erlang
+      # version updates, please review their release notes in case
+      # changes to your application are needed.
+      {:nerves_system_rpi3, "~> 1.18.4", runtime: false, targets: :rpi3},
       {:circuits_gpio, "~> 1.0.1"},
       # {:tzdata, "~> 1.1"},
       {:poison, "~> 5.0", override: true},
@@ -82,10 +71,12 @@ defmodule Drizzle.MixProject do
   def release do
     [
       overwrite: true,
+      # Erlang distribution is not started automatically.
+      # See https://hexdocs.pm/nerves_pack/readme.html#erlang-distribution
       cookie: "#{@app}_cookie",
       include_erts: &Nerves.Release.erts/0,
       steps: [&Nerves.Release.init/1, :assemble],
-      strip_beams: Mix.env() == :prod
+      strip_beams: Mix.env() == :prod or [keep: ["Docs"]]
     ]
   end
 end
